777d3129d4c28553a834dd7643274395cc8a7270
cjs2408 <carlossuero@pursuit.org>
2022-11-20 17:06:28 -0500

    NAVBAR 11.20.22

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+
+function App() {
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route path='/' exact component={Home} />
+		<Route path='/about' component={About} />
+		<Route path='/events' component={Events} />
+		<Route path='/annual' component={AnnualReport} />
+		<Route path='/team' component={Teams} />
+		<Route path='/blogs' component={Blogs} />
+		<Route path='/sign-up' component={SignUp} />
+	</Routes>
+	</Router>
+);
+}
+
+
+
+fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
+
+
+
+export default App;
