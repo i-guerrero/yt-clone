2236aa9fcf39fe8fe14f94c0fbd570e87bdb7845
cjs2408 <carlossuero@pursuit.org>
2022-11-21 19:54:54 -0500

    Preliminary Search Bar, home page etc

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -14,28 +14,13 @@ const Navbar = () => {
     <>
       <Nav>
         <Bars />
-  
         <NavMenu>
+          <NavLink to='/home' activeStyle>
+            Home
+          </NavLink>
           <NavLink to='/about' activeStyle>
             About
           </NavLink>
-          <NavLink to='/events' activeStyle>
-            Events
-          </NavLink>
-          <NavLink to='/annual' activeStyle>
-            Annual Report
-          </NavLink>
-          <NavLink to='/team' activeStyle>
-            Teams
-          </NavLink>
-          <NavLink to='/blogs' activeStyle>
-            Blogs
-          </NavLink>
-          <NavLink to='/sign-up' activeStyle>
-            Sign Up
-          </NavLink>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
         </NavMenu>
         <NavBtn>
           <NavBtnLink to='/signin'>Sign In</NavBtnLink>
---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// return (
+// 	<dix classname="search">
+// 		<form onSubmit={handdleSumit}>
+// 			<label htmlFor="Search"></label>
+// 			<input
+// 				value={search}
+// 				type="text"
+// 				id="search"
+// 				onChange={(e) => setSearch(e.target.value)}
+// 			></input>
+// 			<button>Search</button>
+// 		</form>
+// 	</dix>
+// )
